cfee3bd69f75403dd147811275fa17491ca3cf46
John Alcorn <jalcorn@us.ibm.com>
2021-08-14 16:54:47 -0500

    Migrate to use MicroProfile REST Client

--- src/main/java/com/ibm/hybrid/cloud/sample/stocktrader/notification/NotificationSlack.java
+++ src/main/java/com/ibm/hybrid/cloud/sample/stocktrader/notification/NotificationSlack.java
@@ -16,23 +16,20 @@
 
 package com.ibm.hybrid.cloud.sample.stocktrader.notification;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import com.ibm.hybrid.cloud.sample.stocktrader.notification.json.LoyaltyChange;
+import com.ibm.hybrid.cloud.sample.stocktrader.notification.json.NotificationResult;
+
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.Base64;
+
+//CDI 2.0
+import javax.inject.Inject;
+import javax.enterprise.context.ApplicationScoped;
 
 //Logging (JSR 47)
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
-//JSON-P (JSR 353).  The replaces my old usage of IBM's JSON4J (com.ibm.json.java.JSONObject)
-import javax.json.Json;
-import javax.json.JsonObject;
-import javax.json.JsonObjectBuilder;
 
 //JAX-RS 2.0 (JSR 339)
 import javax.ws.rs.core.Application;
@@ -42,18 +39,37 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Produces;
 import javax.ws.rs.Path;
 
+//mpRestClient 2.0
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
 
 @ApplicationPath("/")
 @Path("/")
+@ApplicationScoped //CDI scope
 /** Determine loyalty status based on total portfolio value.
  *  Also send a notification when status changes for a given user.
  */
 public class NotificationSlack extends Application {
 	private static Logger logger = Logger.getLogger(NotificationSlack.class.getName());
 
+	private @Inject @RestClient OpenWhiskClient openWhiskClient;
+
 	private String url = null;
 	private String id = null;
 	private String pwd = null;
+	private String authorization = null;
+
+	// Override OpenWhisk Client URL if config map is configured to provide URL
+	static {
+		String mpUrlPropName = OpenWhiskClient.class.getName() + "/mp-rest/url";
+		String owURL = System.getenv("OW_URL");
+		if ((owURL != null) && !owURL.isEmpty()) {
+			logger.info("Using OpenWhisk URL from config map: " + owURL);
+			System.setProperty(mpUrlPropName, owURL);
+		} else {
+			logger.info("OpenWhisk URL not found from env var from config map, so defaulting to value in jvm.options: " + System.getProperty(mpUrlPropName));
+		}
+	}
 
 	/**
 	 * Default constructor. 
@@ -97,13 +113,14 @@ public class NotificationSlack extends Application {
 	             imagePullSecrets:
 	             - name: dockerhubsecret
 			 */
-			url = System.getenv("OW_URL");
-			id = System.getenv("OW_ID");
-			pwd = System.getenv("OW_PASSWORD");
+			String id = System.getenv("OW_ID");
+			String pwd = System.getenv("OW_PASSWORD");
 
-			if (url == null) {
-				logger.warning("The OW_URL environment variable was not set!");
+			if (id == null) {
+				logger.warning("The OW_ID environment variable was not set!");
 			} else {
+				String credentials = id + ":" + pwd;
+				authorization = "Basic " + Base64.getEncoder().encodeToString(credentials.getBytes());
 				logger.fine("Initialization completed successfully!");
 			}
 		} catch (Throwable t) {
@@ -117,14 +134,16 @@ public class NotificationSlack extends Application {
 	@Consumes("application/json")
 	@Produces("application/json")
 //	@RolesAllowed({"StockTrader", "StockViewer"}) //Couldn't get this to work; had to do it through the web.xml instead :(
-	public JsonObject notifyLoyaltyLevelChange(JsonObject input) {
-		JsonObjectBuilder result = Json.createObjectBuilder();
+	public NotificationResult notifyLoyaltyLevelChange(LoyaltyChange loyaltyChange) {
+		NotificationResult result = new NotificationResult();
 		String message = null;
 
-		if (input != null) try {
+		if (loyaltyChange != null) try {
 			logger.fine("Notifying about change in loyalty level");
-			invokeREST("POST", url, input.toString(), id, pwd);
-			message = input.getString("owner")+" has changed status from "+input.getString("old")+" to "+input.getString("new")+".";
+
+			openWhiskClient.sendSlackMessageViaOpenWhisk(authorization, loyaltyChange);
+
+			message = loyaltyChange.getOwner()+" has changed status from "+loyaltyChange.getOld()+" to "+loyaltyChange.getNew()+".";
 			logger.fine("Successfully delivered message to IBM Cloud Functions");
 		} catch (Throwable t) { //in case OpenWhisk credentials are not configured, just log the exception and continue
 			logger.warning("An unexpected error occurred.  Continuing without notification of change in loyalty level.");
@@ -134,49 +153,10 @@ public class NotificationSlack extends Application {
 			message = "No http body provided in call to Notification microservice!";
 		}
 
-		result.add("message", message);
-		result.add("location", "Slack");
+		result.setMessage(message);
+		result.setLocation("Slack");
 
-		return result.build();
-	}
-
-	private static JsonObject invokeREST(String verb, String uri, String input, String user, String password) throws IOException {
-		logger.fine("Preparing to invoke "+verb+" on "+uri);
-		URL url = new URL(uri);
-
-		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-		conn.setRequestMethod(verb);
-		conn.setRequestProperty("Content-Type", "application/json");
-		conn.setDoOutput(true);
-
-		if ((user != null) && (password != null)) { //send via basic-auth
-			logger.fine("Setting credentials on REST call");
-			String credentials = user + ":" + password;
-			String authorization = "Basic " + Base64.getEncoder().encodeToString(credentials.getBytes());
-			conn.setRequestProperty("Authorization", authorization);
-			logger.fine("Successfully set credentials");
-		}
-
-		if (input != null) {
-			logger.fine("Writing JSON to body of REST call");
-			OutputStream body = conn.getOutputStream();
-			body.write(input.getBytes());
-			body.flush();
-			body.close();
-			logger.fine("Successfully wrote JSON");
-		}
-
-		logger.fine("Driving call to REST API");
-		InputStream stream = conn.getInputStream();
-
-		logger.fine("Parsing results as JSON");
-//		JSONObject json = JSONObject.parse(stream); //JSON4J
-		JsonObject json = Json.createReader(stream).readObject();
-
-		stream.close();
-
-		logger.fine("Returning JSON to caller of REST API");
-		return json;
+		return result;
 	}
 
 	private static void logException(Throwable t) {
