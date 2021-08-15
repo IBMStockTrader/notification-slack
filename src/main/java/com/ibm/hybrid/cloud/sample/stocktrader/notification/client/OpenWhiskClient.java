8b20ab0e9591cd87abb5fc1981b1d72d22f30f9e
John Alcorn <jalcorn@us.ibm.com>
2021-08-14 22:17:31 -0500

    Add consumes application/json

--- src/main/java/com/ibm/hybrid/cloud/sample/stocktrader/notification/client/OpenWhiskClient.java
+++ src/main/java/com/ibm/hybrid/cloud/sample/stocktrader/notification/client/OpenWhiskClient.java
@@ -21,9 +21,10 @@ import com.ibm.hybrid.cloud.sample.stocktrader.notification.json.NotificationRes
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.POST;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
+import javax.ws.rs.POST;
 import javax.ws.rs.Produces;
 
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
